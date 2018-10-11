6607b200f747775507a1941423eb844a126f5478
Esther <esther2180@gmail.com>
2018-10-11 14:41:35 -0700

    Get movie quote complete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import userReducer from './user_reducer';
-import movieReducer from './user_reducer';
+import movieReducer from './movie_reducer';
 
 const rootReducer = combineReducers({
     form: formReducer,
